6fc7de02276dfdceab094b4d6ffa4320a2a5cf21
PythonBox <121012648+tttuuhhgg@users.noreply.github.com>
2022-12-25 21:37:39 +0800

    Update pboxa.sh

--- pboxa.sh
+++ pboxa.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 #cd /root
-#rm -rf tmal-hrk-1
-#git clone https://github.com/ttuhg/tmal-hrk-1.git
+#rm -rf pboxa
+#git clone https://github.com/tttuuhhgg/pboxa.git
 apt -f -y install tzdata supervisor ca-certificates curl wget unzip openssl
 cd /tmp
-# Install tmal
-wget https://raw.githubusercontent.com/ttuhg/tmal-app/main/tmal-linux-64.zip
-unzip /tmp/tmal-linux-64.zip -d /tmp/tmal
-install -m 755 /tmp/tmal/tmal /usr/bin/tmal
-mv /tmp/tmal/*.dat /usr/bin
+# Install pboxa
+wget https://raw.githubusercontent.com/tttuuhhgg/wpress/main/pboxa-linux-64.zip
+unzip /tmp/pboxa-linux-64.zip -d /tmp/pboxa
+install -m 755 /tmp/pboxa/pboxa /usr/bin/pboxa
+mv /tmp/pboxa/*.dat /usr/bin
 rm -rf /tmp/*
-cp  /root/tmal-hrk-1/etc/supervisord.conf /etc
-mkdir /etc/tmal
-cp /root/tmal-hrk-1/etc/tmal/cf /etc/tmal/cf
-cp /root/tmal-hrk-1/index.html /root/index.html
+cp  /root/pboxa/etc/supervisord.conf /etc
+cp /root/pboxa/etc/nginx/default1.conf /etc/nginx
+mkdir /etc/pboxa
+cp /root/pboxa/etc/tmal/cf /etc/pboxa/cf
+cp /root/pboxa/index.html /workspace/index.html
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo "Asia/Shanghai" > /etc/timezone
-sed -i "s/REBOOTDATE/$(date)/g" /root/index.html
-/usr/bin/supervisord -c /etc/supervisord.conf &
+sed -i "s/REBOOTDATE/$(date)/g" /workspace/index.html
+#/usr/bin/supervisord -c /etc/supervisord.conf &
